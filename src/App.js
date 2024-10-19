f7be17db5a82810ff8bafff994b9d5f4f0874ee7
reemaz al soufe <remoso88@gmail.com>
2024-10-19 19:49:29 +0400

    update build 2

--- src/App.js
+++ src/App.js
@@ -14,22 +14,36 @@ const App = () => {
 
   return (
     <Router>
-      <div className="flex min-h-screen justify-center bg-customBlue p-8 ">
+      <div className='flex min-h-screen justify-center bg-customBlue p-8 '>
         {/* Wrapper div with fixed width, height, and margin-top */}
-        <div className="flex w-[58.5rem] h-[37.5rem] bg-white rounded-xl shadow-md mt-20 z-10">
+        <div className='flex w-[58.5rem] h-[37.5rem] bg-white rounded-xl shadow-md mt-20 z-10'>
           {/* Sidebar on the left */}
-          <div className="w-1/3 p-4 bg-white bg-opacity-0 rounded-l-lg h-full z-1">
-          <Sidebar currentStep={currentStep} />
+          <div className='w-1/3 p-4 bg-white bg-opacity-0 rounded-l-lg h-full z-1'>
+            <Sidebar currentStep={currentStep} />
           </div>
 
           {/* Content (routes) on the right */}
-          <div className="w-2/3 p-8 h-full ">
+          <div className='w-2/3 p-8 h-full '>
             <Routes>
-              <Route path="/" element={<StepOne setCurrentStep={setCurrentStep} />} />
-              <Route path="/step2" element={<StepTwo setCurrentStep={setCurrentStep} />} />
-              <Route path="/step3" element={<StepThree setCurrentStep={setCurrentStep} />} />
-              <Route path="/step4" element={<StepFour setCurrentStep={setCurrentStep} />} />
-              <Route path="/step5" element={<StepFive />} /> {/* New route for Step 5 */}
+              <Route
+                default={true}
+                path='/'
+                element={<StepOne setCurrentStep={setCurrentStep} />}
+              />
+              <Route
+                path='/step2'
+                element={<StepTwo setCurrentStep={setCurrentStep} />}
+              />
+              <Route
+                path='/step3'
+                element={<StepThree setCurrentStep={setCurrentStep} />}
+              />
+              <Route
+                path='/step4'
+                element={<StepFour setCurrentStep={setCurrentStep} />}
+              />
+              <Route path='/step5' element={<StepFive />} />{' '}
+              {/* New route for Step 5 */}
             </Routes>
           </div>
         </div>
@@ -39,4 +53,3 @@ const App = () => {
 };
 
 export default App;
-
